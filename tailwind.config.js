5ec3a6118bcf4e89065eda09f114f4d8eb5d9147
MattDorling <dorling.mj@gmail.com>
2021-09-18 00:49:25 +0100

    Add some colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#57534E',
+        secondary: '#3F6212',
+        tertiary: '#E4E1C9',
+        error: '#DC2626',
+      },
+    },
   },
   variants: {
     extend: {},
