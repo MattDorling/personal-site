88999f7282c738662c28b506ee15e666b6250e1b
MattDorling <dorling.mj@gmail.com>
2021-09-18 13:58:34 +0100

    Add dark colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         primary: '#57534E',
         secondary: '#3F6212',
         tertiary: '#E4E1C9',
+        'tertiary-dark': '#3E3D3A',
         error: '#DC2626',
       },
     },
